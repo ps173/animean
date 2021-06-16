355a0eb47a342befb5bd4d0fbff2b2214e25113e
ps173 <prathamsharma173@gmail.com>
2021-06-16 23:24:23 +0530

    Commited Changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home, SignIn, Login, Browse } from "./pages";
 import * as ROUTES from "./constants/routes";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/Routes";
 
 function App() {
-  const user = null;
+    const user = null;
 
-  return (
-    <Router>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.LOGIN}>
-        <Login />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
-        <Browse />
-      </Route>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={ROUTES.SIGN_UP}>
+                    <SignIn />
+                </Route>
+                <Route exact path={ROUTES.LOGIN}>
+                    <Login />
+                </Route>
+                <Route exact path={ROUTES.HOME}>
+                    <Home />
+                </Route>
+                <Route exact path={ROUTES.BROWSE}>
+                    <Browse />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
